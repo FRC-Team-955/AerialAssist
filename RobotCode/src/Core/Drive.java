db148264a006e8ccbb320c302e93f0b7545ad1e7
raiderbot-2 <raiderbot-2@raiderbot2-Linux>
2014-01-21 17:36:35 -0800

    Added macro and  diagnostics

--- RobotCode/src/Core/Drive.java
+++ RobotCode/src/Core/Drive.java
@@ -3,44 +3,45 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package Core;
+
 import ModClasses.MyTalon;
 import ModClasses.MyJoystick;
 import Utils.*;
+
 /**
  *
  * @author raiderbot-4
  */
 public class Drive {
-    MyTalon left1 = new MyTalon(Config.Drive.chnLeft1);
-    MyTalon left2 = new MyTalon(Config.Drive.chnLeft2);
-    MyTalon left3 = new MyTalon(Config.Drive.chnLeft3);
-    
-    MyTalon right1 = new MyTalon(Config.Drive.chnRight1);
-    MyTalon right2 = new MyTalon(Config.Drive.chnRight2);
-    MyTalon right3 = new MyTalon(Config.Drive.chnRight3);
-    
-    public void setSpeed(MyJoystick joy){
+
+    public MyTalon left1 = new MyTalon(Config.Drive.chnLeft1);
+    public MyTalon left2 = new MyTalon(Config.Drive.chnLeft2);
+    public MyTalon left3 = new MyTalon(Config.Drive.chnLeft3);
+
+    public MyTalon right1 = new MyTalon(Config.Drive.chnRight1);
+    public MyTalon right2 = new MyTalon(Config.Drive.chnRight2);
+    public MyTalon right3 = new MyTalon(Config.Drive.chnRight3);
+
+    public void setSpeed(MyJoystick joy) {
         double x = joy.getX();
         double y = joy.getY();
         setSpeed(y + x, y - x);
     }
-    
-    public void setLeft(double speed){
+
+    public void setLeft(double speed) {
         left1.set(speed);
         left2.set(speed);
         left3.set(speed);
     }
-    
-    public void setRight(double speed){
+
+    public void setRight(double speed) {
         right1.set(speed);
         right2.set(speed);
         right3.set(speed);
     }
-    
-    public void setSpeed(double left, double right)
-    {
+
+    public void setSpeed(double left, double right) {
         setLeft(left);
         setRight(right);
     }
---
+++ RobotCode/src/diag/Macro.java
@@ -0,0 +1,78 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package diag;
+
+import Core.Drive;
+import ModClasses.MyJoystick;
+import edu.wpi.first.wpilibj.Joystick;
+
+/**
+ *
+ * @author raiderbot-2
+ */
+public class Macro {
+    
+    MyJoystick joy;
+    Drive drive;
+            
+    public Macro(MyJoystick joy, Drive drive){
+    
+    this.drive = drive;
+    this.joy = joy;
+    
+    }
+    
+    public void run(){
+    
+    testJoysticks();
+    testMotors();
+    
+    
+    
+    }
+    
+    
+    public void testMotors(){
+    
+   try {
+    drive.left1.set(1);
+    drive.left2.set(1);
+    drive.left3.set(1);
+    
+    drive.right1.set(1);
+    drive.right2.set(1);
+    drive.right3.set(1);
+   }
+    
+   
+   catch(Exception exception){
+   
+   System.err.print("Error in motors");
+   
+   
+   }
+    }
+    
+    public void testJoysticks(){
+    try{
+    joy.getX();
+    joy.getY();
+    joy.getAxis(Joystick.AxisType.kX);
+    joy.getBumper();
+    }
+    
+    
+    catch(Exception exception){
+    
+    
+        System.err.println("Error in Joystick");
+    
+    }
+    }
+    
+    
+}

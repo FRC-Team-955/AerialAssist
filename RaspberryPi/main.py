016fb015ba7f8bb9909ca71c017348fbc2b93174
Merfoo <fauzimerfoo@gmail.com>
2014-02-02 08:40:48 +0000

    check network table in try catch block

--- RaspberryPi/main.py
+++ RaspberryPi/main.py
@@ -14,15 +14,15 @@ import time
 # is run because it is imported, then __name__ is the module name.
 
 if __name__ == '__main__': 
-        logging.basicConfig(filename='/var/log/robovision/robovision.log',level=logging.DEBUG)
-        logging.debug("vision services started.")
-        logging.debug("uid {} euid {}".format(os.getuid(), os.geteuid()))
+	logging.basicConfig(filename='/var/log/robovision/robovision.log',level=logging.DEBUG)
+	logging.debug("vision services started.")
+	#logging.debug("uid {} euid {}".format(os.getuid(), os.geteuid()))
 
 	# Table name  = team number, used for ip address
 	tableName = "955"
 	#table = NetworkTableClient(tableName)
 	tableDirectory = '/' + tableName + '/'
-
+	logging.debug("Network table initialized.")
 	# Id's for reading data from networktable
 	isGoalHotId = "isGoalHot"
 	shutDownPi = "shutdown"
@@ -30,18 +30,22 @@ if __name__ == '__main__':
 	runVisionId = "runVision"
 	prefSideLeftId = "prefSideLeft"
 
-	table.setValue(tableDirectory + isGoalHotId, False) # Set foundHotTarget
+	try:
+		table.setValue(tableDirectory + isGoalHotId, False) # Set foundHotTarget
 
-	while True:
-		runVision = table.getValue(tableDirectory + runVisionId)
-		debugMode = table.getValue(tableDirectory + debugId)
-		prefSideLeft = table.getValue(tableDirectory + prefSideLeftId)
+		while True:
+			runVision = table.getValue(tableDirectory + runVisionId)
+			debugMode = table.getValue(tableDirectory + debugId)
+			prefSideLeft = table.getValue(tableDirectory + prefSideLeftId)
 
-		if runVision: # Check if runVision is true
-			vision.update(prefSideLeft, debugMode)	# Run vision, check whether debug mode is true
-			table.setValue(tableDirectory + isGoalHotId, vision.getFoundHotTarget()) # Set foundHotTarget
+			if runVision: # Check if runVision is true
+				vision.update(prefSideLeft, debugMode)	# Run vision, check whether debug mode is true
+				table.setValue(tableDirectory + isGoalHotId, vision.getFoundHotTarget()) # Set foundHotTarget
 
-		if table.getValue(tableDirectory + shutDownPi):
-			os.sys("sudo shutdown -h now")
+			if table.getValue(tableDirectory + shutDownPi):
+				os.sys("sudo shutdown -h now")
 
-		time.sleep(1.0 / 4.0)
+			time.sleep(1.0 / 4.0)
+	except Exception as ex:
+		logging.error(ex)
+	logging.debug("Done!")
